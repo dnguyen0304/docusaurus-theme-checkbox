e43d3a86c309683b547875b0e1b193a7a3027260
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-20 16:43:53 -0700

    Fix using double quotes for LinearProgress component.

--- src/theme/docupotamus-task-list/components/LinearProgress/index.tsx
+++ src/theme/docupotamus-task-list/components/LinearProgress/index.tsx
@@ -16,10 +16,10 @@ export default function LinearProgress(props: Props): JSX.Element {
             alignItems: 'center',
         }}>
             <Box sx={{ width: '100%', mr: 1 }}>
-                <MuiLinearProgress variant="determinate" {...props} />
+                <MuiLinearProgress variant='determinate' {...props} />
             </Box>
             <Box sx={{ minWidth: 35 }}>
-                <Typography variant="body2" color="text.secondary">{`${Math.round(
+                <Typography variant='body2' color='text.secondary'>{`${Math.round(
                     props.value,
                 )}%`}</Typography>
             </Box>
