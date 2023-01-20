04251669a9b610471b17659d73dd6369b338f946
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-20 16:01:08 -0700

    Add LinearProgress component.

---
+++ src/theme/docupotamus-task-list/components/LinearProgress/index.tsx
@@ -0,0 +1,29 @@
+// Copied from: https://mui.com/material-ui/react-progress/#linear-with-label
+
+import Box from '@mui/material/Box';
+import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+
+function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ width: '100%', mr: 1 }}>
+                <LinearProgress variant="determinate" {...props} />
+            </Box>
+            <Box sx={{ minWidth: 35 }}>
+                <Typography variant="body2" color="text.secondary">{`${Math.round(
+                    props.value,
+                )}%`}</Typography>
+            </Box>
+        </Box>
+    );
+}
+
+export default function LinearWithValueLabel({ progress }) {
+    return (
+        <Box sx={{ width: '100%' }}>
+            <LinearProgressWithLabel value={progress} />
+        </Box>
+    );
+}
