9ead68078a8fe8e35c359b52b443659b1a79b1aa
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-20 16:35:40 -0700

    Rename default export for LinearProgress component.

--- src/theme/docupotamus-task-list/components/LinearProgress/index.tsx
+++ src/theme/docupotamus-task-list/components/LinearProgress/index.tsx
@@ -1,7 +1,7 @@
 // Copied from: https://mui.com/material-ui/react-progress/#linear-with-label
 
 import Box from '@mui/material/Box';
-import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
+import MuiLinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
 
@@ -9,7 +9,7 @@ function LinearProgressWithLabel(props: LinearProgressProps & { value: number })
     return (
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Box sx={{ width: '100%', mr: 1 }}>
-                <LinearProgress variant="determinate" {...props} />
+                <MuiLinearProgress variant="determinate" {...props} />
             </Box>
             <Box sx={{ minWidth: 35 }}>
                 <Typography variant="body2" color="text.secondary">{`${Math.round(
@@ -20,7 +20,7 @@ function LinearProgressWithLabel(props: LinearProgressProps & { value: number })
     );
 }
 
-export default function LinearWithValueLabel({ progress }) {
+export default function LinearProgress({ progress }) {
     return (
         <Box sx={{ width: '100%' }}>
             <LinearProgressWithLabel value={progress} />
