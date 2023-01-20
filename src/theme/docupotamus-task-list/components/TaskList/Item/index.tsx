34aba03b36ef474a43314cc348ec2439de2ab5ae
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-20 16:30:07 -0700

    Refactor to use Item for TaskList component.

---
+++ src/theme/docupotamus-task-list/components/TaskList/Item/index.tsx
@@ -0,0 +1,98 @@
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CircleCheckedFilled from '@mui/icons-material/CheckCircle';
+import CircleUnchecked from '@mui/icons-material/RadioButtonUnchecked';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel, { FormControlLabelProps } from '@mui/material/FormControlLabel';
+import { styled } from '@mui/material/styles';
+import * as React from 'react';
+import useThemeConfig from '../../../hooks/useThemeConfig';
+
+const StyledFormControlLabel = styled(FormControlLabel)({
+    borderRadius: '0.5rem',
+    paddingTop: '0.2rem',
+    paddingBottom: '0.2rem',
+    '&:hover': {
+        backgroundColor: 'var(--ifm-hover-overlay)',
+    },
+    '&:hover :not(.Mui-checked) + .MuiFormControlLabel-label': {
+        color: 'var(--ifm-color-primary)',
+        transition:
+            `color `
+            + `var(--ifm-transition-fast) `
+            + `var(--ifm-transition-timing-default)`,
+    },
+    '& .Mui-checked + .MuiFormControlLabel-label': {
+        color: 'var(--ifm-color-emphasis-600)',
+        textDecorationLine: 'line-through',
+    },
+});
+
+interface Props extends Pick<FormControlLabelProps, 'label'> {
+    readonly setIsCheckedCount: React.Dispatch<React.SetStateAction<number>>
+};
+
+export default function TaskList(
+    {
+        label,
+        setIsCheckedCount,
+    }: Props,
+): JSX.Element {
+    const {
+        checkbox: {
+            color,
+            shape,
+            size,
+        },
+    } = useThemeConfig();
+
+    const [isChecked, setIsChecked] = React.useState<boolean>(false);
+    const [iconChecked, setIconChecked] =
+        React.useState<JSX.Element>(<CheckBoxIcon />);
+    const [iconNotChecked, setIconNotChecked] =
+        React.useState<JSX.Element>(<CheckBoxOutlineBlankIcon />);
+
+    const handleChange = () => {
+        setIsChecked(prev => {
+            const newIsChecked = !prev;
+            if (newIsChecked) {
+                setIsCheckedCount(prev => prev + 1);
+            } else {
+                setIsCheckedCount(prev => prev > 0 ? prev - 1 : 0);
+            }
+            return newIsChecked;
+        });
+    };
+
+    React.useEffect(() => {
+        if (shape === 'circle') {
+            setIconChecked(<CircleCheckedFilled />);
+            setIconNotChecked(<CircleUnchecked />);
+        }
+        if (shape === 'square') {
+            setIconChecked(<CheckBoxIcon />);
+            setIconNotChecked(<CheckBoxOutlineBlankIcon />);
+        }
+    }, [shape]);
+
+    return (
+        <StyledFormControlLabel
+            checked={isChecked}
+            control={
+                <Checkbox
+                    icon={iconNotChecked}
+                    checkedIcon={iconChecked}
+                    size={size}
+                    sx={{
+                        color: 'currentColor',
+                        '&.Mui-checked': {
+                            color,
+                        },
+                    }}
+                />
+            }
+            label={label}
+            onChange={handleChange}
+        />
+    );
+};
--- src/theme/docupotamus-task-list/components/TaskList/index.tsx
+++ src/theme/docupotamus-task-list/components/TaskList/index.tsx
@@ -1,43 +1,15 @@
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import CircleCheckedFilled from '@mui/icons-material/CheckCircle';
-import CircleUnchecked from '@mui/icons-material/RadioButtonUnchecked';
 import Box from '@mui/material/Box';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
 import { styled } from '@mui/material/styles';
 import * as React from 'react';
-import useThemeConfig from '../../hooks/useThemeConfig';
 import LinearProgress from '../LinearProgress';
+import TaskItem from './Item';
 
 const DELIMITER: string = '- [ ] ';
-
-type onChange = (event: React.SyntheticEvent, checked: boolean) => void;
 
 const StyledFormGroup = styled(FormGroup)({
     marginBottom: 'var(--ifm-leading)',
     paddingLeft: '1rem',
-});
-
-const StyledFormControlLabel = styled(FormControlLabel)({
-    borderRadius: '0.5rem',
-    paddingTop: '0.2rem',
-    paddingBottom: '0.2rem',
-    '&:hover': {
-        backgroundColor: 'var(--ifm-hover-overlay)',
-    },
-    '&:hover :not(.Mui-checked) + .MuiFormControlLabel-label': {
-        color: 'var(--ifm-color-primary)',
-        transition:
-            `color `
-            + `var(--ifm-transition-fast) `
-            + `var(--ifm-transition-timing-default)`,
-    },
-    '& .Mui-checked + .MuiFormControlLabel-label': {
-        color: 'var(--ifm-color-emphasis-600)',
-        textDecorationLine: 'line-through',
-    },
 });
 
 // Copied from: https://github.com/facebook/docusaurus/blob/a308fb7c81832cca354192fe2984f52749441249/packages/docusaurus-theme-classic/src/theme/CodeBlock/index.tsx#L20
@@ -63,54 +35,17 @@ export default function TaskList(
         children: rawChildren,
     }: Props,
 ): JSX.Element {
-    const {
-        checkbox: {
-            color,
-            shape,
-            size,
-        },
-    } = useThemeConfig();
-
     const [labels, setLabels] = React.useState<string[]>([]);
-    const [isCheckeds, setIsCheckeds] = React.useState<boolean[]>([]);
-    const [iconChecked, setIconChecked] =
-        React.useState<JSX.Element>(<CheckBoxIcon />);
-    const [iconNotChecked, setIconNotChecked] =
-        React.useState<JSX.Element>(<CheckBoxOutlineBlankIcon />);
     const [progress, setProgress] = React.useState<number>(0);
-
-    const createHandleChange = (i: number): onChange => {
-        return (() => setIsCheckeds(prev => {
-            if (i >= prev.length) {
-                throw new Error('index out of bounds');
-            }
-            const copied = [...prev];
-            copied[i] = !copied[i];
-            return copied;
-        }));
-    };
+    const [isCheckedCount, setIsCheckedCount] = React.useState<number>(0);
 
     React.useEffect(() => {
-        if (shape === 'circle') {
-            setIconChecked(<CircleCheckedFilled />);
-            setIconNotChecked(<CircleUnchecked />);
-        }
-        if (shape === 'square') {
-            setIconChecked(<CheckBoxIcon />);
-            setIconNotChecked(<CheckBoxOutlineBlankIcon />);
-        }
-    }, [shape]);
-
-    React.useEffect(() => {
-        const completed = isCheckeds
-            .map<number>(isChecked => isChecked ? 1 : 0)
-            .reduce(
-                (accumulator, currentValue) => accumulator + currentValue,
-                0,
-            );
-        const total = isCheckeds.length;
-        setProgress(Math.floor(completed / total * 100));
-    }, [isCheckeds]);
+        const newProgress =
+            (labels.length)
+                ? Math.floor(isCheckedCount / labels.length * 100)
+                : 0;
+        setProgress(newProgress);
+    }, [isCheckedCount]);
 
     React.useEffect(() => {
         const children = stringifyChildren(rawChildren);
@@ -119,7 +54,6 @@ export default function TaskList(
                 .split(DELIMITER)
                 .filter(item => item.length);
         setLabels(newLabels);
-        setIsCheckeds(Array(newLabels.length).fill(false));
     }, []);
 
     return (
@@ -127,33 +61,13 @@ export default function TaskList(
             <LinearProgress progress={progress} />
             <StyledFormGroup>
                 {labels.map((label, i) => {
-                    const controlledProps =
-                        (i < isCheckeds.length)
-                            ? {
-                                checked: isCheckeds[i],
-                                onChange: createHandleChange(i),
-                            }
-                            : {};
                     return (
-                        <StyledFormControlLabel
+                        <TaskItem
                             // If items are modified, update how the key is
                             // generated.
                             key={`taskItem-${i}`}
-                            control={
-                                <Checkbox
-                                    icon={iconNotChecked}
-                                    checkedIcon={iconChecked}
-                                    size={size}
-                                    sx={{
-                                        color: 'currentColor',
-                                        '&.Mui-checked': {
-                                            color,
-                                        },
-                                    }}
-                                />
-                            }
                             label={label}
-                            {...controlledProps}
+                            setIsCheckedCount={setIsCheckedCount}
                         />
                     );
                 })}
