b252ddbc3bfaf9cc30789f502d7567a9b503e9da
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-20 16:53:15 -0700

    Use theme config color for LinearProgress component.

--- src/theme/docupotamus-task-list/components/LinearProgress/index.tsx
+++ src/theme/docupotamus-task-list/components/LinearProgress/index.tsx
@@ -4,20 +4,34 @@ import Box from '@mui/material/Box';
 import MuiLinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
+import useThemeConfig from '../../hooks/useThemeConfig';
 
 interface Props extends LinearProgressProps {
     readonly value: number;
 };
 
 export default function LinearProgress(props: Props): JSX.Element {
+    const {
+        progressBar: {
+            color,
+        },
+    } = useThemeConfig();
+
     return (
         <Box sx={{
             display: 'flex',
             alignItems: 'center',
             gap: '0.5rem',
         }}>
-            <Box sx={{ width: '100%' }}>
-                <MuiLinearProgress variant='determinate' {...props} />
+            <Box sx={{
+                color,
+                width: '100%',
+            }}>
+                <MuiLinearProgress
+                    color='inherit'
+                    variant='determinate'
+                    {...props}
+                />
             </Box>
             <Box sx={{ minWidth: 35 }}>
                 <Typography variant='body2' color='text.secondary'>{`${Math.round(
