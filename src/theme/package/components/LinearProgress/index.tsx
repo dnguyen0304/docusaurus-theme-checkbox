3f7ef081df530b2cf83284b77897bfe6b64373c0
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-13 05:08:56 -0700

    Fix background-color for LinearProgress styles.

--- src/theme/package/components/LinearProgress/index.tsx
+++ src/theme/package/components/LinearProgress/index.tsx
@@ -1,8 +1,25 @@
 import Box from '@mui/material/Box';
 import MuiLinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
+import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import useTaskListThemeConfig from '../../hooks/useTaskListThemeConfig';
+
+interface StyledBoxProps {
+    readonly barColor: React.CSSProperties['color'];
+};
+
+const StyledBox = styled(Box, {
+    shouldForwardProp: (prop) => prop !== 'barColor'
+})<StyledBoxProps>(({ barColor }) => ({
+    width: '100%',
+    '&.MuiBox-root .MuiLinearProgress-bar': {
+        backgroundColor: barColor,
+    },
+    '&.MuiBox-root .MuiLinearProgress-root::before': {
+        backgroundColor: barColor,
+    },
+}));
 
 interface Props extends LinearProgressProps {
     readonly value: number;
@@ -11,7 +28,7 @@ interface Props extends LinearProgressProps {
 export default function LinearProgress(props: Props): JSX.Element {
     const {
         progressBar: {
-            color,
+            color: barColor,
         },
     } = useTaskListThemeConfig();
 
@@ -21,16 +38,15 @@ export default function LinearProgress(props: Props): JSX.Element {
             alignItems: 'center',
             gap: '0.5rem',
         }}>
-            <Box sx={{
-                color,
-                width: '100%',
-            }}>
+            <StyledBox barColor={barColor}>
                 <MuiLinearProgress
+                    // This is a fallback. Set color in the styled component for
+                    // higher specificity.
                     color='inherit'
                     variant='determinate'
                     {...props}
                 />
-            </Box>
+            </StyledBox>
             <Box sx={{ minWidth: '10%' }}>
                 <Typography variant='body2'>
                     {`${Math.round(props.value)}%`}
