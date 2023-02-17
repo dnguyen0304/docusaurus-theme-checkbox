9fb5f1e1c524836b51b42484db75ac4b7421a18c
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-17 06:44:31 -0700

    Add WorkbenchTab component.

---
+++ src/theme/package/components/TaskList/WorkbenchTab/index.tsx
@@ -0,0 +1,26 @@
+import { useLocation } from '@docusaurus/router';
+import Box from '@mui/material/Box';
+import * as React from 'react';
+import { useTasks } from '../../../contexts/tasks';
+import List from '../List';
+
+export default function WorkbenchTab(): JSX.Element {
+    const location = useLocation();
+    const { tasks } = useTasks();
+
+    return (
+        <Box>
+            {Array
+                .from(tasks.get(location.pathname)?.keys() ?? [])
+                .map(taskListId => {
+                    return (
+                        <List
+                            path={location.pathname}
+                            taskListId={taskListId}
+                        />
+                    );
+                })
+            }
+        </Box>
+    );
+};
