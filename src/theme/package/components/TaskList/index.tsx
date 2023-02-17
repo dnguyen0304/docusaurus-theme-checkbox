9cf4c045c716d9a3d8cb294adfb215bc832729b4
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-17 03:09:44 -0700

    Extract to useLabelsParsed hook for TaskList component.

--- src/theme/package/components/TaskList/index.tsx
+++ src/theme/package/components/TaskList/index.tsx
@@ -1,21 +1,6 @@
 import * as React from 'react';
+import useLabelsParsed from '../../hooks/useLabelsParsed';
 import List from './List';
-
-const DELIMITER: string = '- [ ] ';
-
-// Copied from: https://github.com/facebook/docusaurus/blob/a308fb7c81832cca354192fe2984f52749441249/packages/docusaurus-theme-classic/src/theme/CodeBlock/index.tsx#L20
-const stringifyChildren = (children: React.ReactNode): string => {
-    const hasElement =
-        React.Children
-            .toArray(children)
-            .some((child) => React.isValidElement(child));
-    if (hasElement) {
-        throw new Error(
-            'rendering non-text nodes in a task list is not yet supported'
-        );
-    }
-    return Array.isArray(children) ? children.join('') : (children as string);
-};
 
 interface Props {
     readonly children: React.ReactNode;
@@ -26,16 +11,7 @@ export default function TaskList(
         children,
     }: Props,
 ): JSX.Element {
-    const [labels, setLabels] = React.useState<string[]>([]);
-
-    React.useEffect(() => {
-        const stringified = stringifyChildren(children);
-        const newLabels =
-            stringified
-                .split(DELIMITER)
-                .filter(item => item.length);
-        setLabels(newLabels);
-    }, []);
+    const labels = useLabelsParsed(children);
 
     return (
         <List labels={labels} />
