d22c9d433f488a4935d8671855b4f789c47dc8bc
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-18 07:26:49 -0700

    Add Layout for WorkbenchTab component.

--- src/theme/package/components/WorkbenchTab/index.tsx
+++ src/theme/package/components/WorkbenchTab/index.tsx
@@ -1,15 +1,26 @@
 import { useLocation } from '@docusaurus/router';
 import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 import * as React from 'react';
 import { useTasks } from '../../contexts/tasks';
 import List from '../TaskList/List';
+
+const Layout = styled(Box)({
+    height: '100%',
+
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+
+    padding: 'var(--d9s-space-m)',
+});
 
 export default function WorkbenchTab(): JSX.Element {
     const location = useLocation();
     const { tasks } = useTasks();
 
     return (
-        <Box>
+        <Layout>
             {Array
                 .from(tasks.get(location.pathname)?.keys() ?? [])
                 .map(taskListId => {
@@ -21,6 +32,6 @@ export default function WorkbenchTab(): JSX.Element {
                     );
                 })
             }
-        </Box>
+        </Layout>
     );
 };
