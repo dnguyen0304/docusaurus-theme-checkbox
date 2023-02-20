e30a2539ecc623d2801364ded3173e6120f62b7e
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-20 07:07:54 -0700

    Extract to Tabs for WorkbenchTab component.

---
+++ src/theme/package/components/WorkbenchTab/Tabs/index.tsx
@@ -0,0 +1,32 @@
+import { useLocation } from '@docusaurus/router';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import * as React from 'react';
+import { useTasks } from '../../../contexts/tasks';
+import List from '../../TaskList/List';
+
+const Layout = styled(Box)({
+    display: 'flex',
+    flexDirection: 'row',
+});
+
+export default function WorkbenchTab(): JSX.Element {
+    const location = useLocation();
+    const { tasks } = useTasks();
+
+    return (
+        <Layout>
+            {Array
+                .from(tasks.get(location.pathname)?.keys() ?? [])
+                .map(taskListId => {
+                    return (
+                        <List
+                            path={location.pathname}
+                            taskListId={taskListId}
+                        />
+                    );
+                })
+            }
+        </Layout>
+    );
+};
--- src/theme/package/components/WorkbenchTab/index.tsx
+++ src/theme/package/components/WorkbenchTab/index.tsx
@@ -1,10 +1,8 @@
-import { useLocation } from '@docusaurus/router';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import * as React from 'react';
-import { useTasks } from '../../contexts/tasks';
-import List from '../TaskList/List';
 import ButtonGroup from './ButtonGroup';
+import Tabs from './Tabs';
 
 const Layout = styled(Box)({
     height: '100%',
@@ -18,22 +16,9 @@ const Layout = styled(Box)({
 });
 
 export default function WorkbenchTab(): JSX.Element {
-    const location = useLocation();
-    const { tasks } = useTasks();
-
     return (
         <Layout>
-            {Array
-                .from(tasks.get(location.pathname)?.keys() ?? [])
-                .map(taskListId => {
-                    return (
-                        <List
-                            path={location.pathname}
-                            taskListId={taskListId}
-                        />
-                    );
-                })
-            }
+            <Tabs />
             <ButtonGroup />
         </Layout>
     );
